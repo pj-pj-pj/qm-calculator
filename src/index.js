2996d47249361d7e951ed4d8eb466fb043b68ae2
pj-01 <96671928+pj-pj-pj@users.noreply.github.com>
2023-11-01 01:03:42 +0800

    make buttons clickable - also refactored some variable names (index.js)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 // for the responsive sidebar, show on click then hide
-const calculatorsBtn = document.querySelector('nav #nav-button');
-const sideBar = document.querySelector('aside');
+const menuBtn = document.querySelector('nav #nav-button');
+const sidebar = document.querySelector('aside');
 
 function windowOnClick(event) {
-  if (!sideBar.contains(event.target) && !calculatorsBtn.contains(event.target) && !sideBar.classList.contains('hidden')) {
-    sideBar.classList.add('hidden');
+  if (!sidebar.contains(event.target) && !menuBtn.contains(event.target) && !sidebar.classList.contains('hidden')) {
+    sidebar.classList.add('hidden');
     window.removeEventListener('click', windowOnClick);
   }
 }
 
-calculatorsBtn.onclick = () => {
-  sideBar.classList.toggle('hidden');
+menuBtn.onclick = () => {
+  sidebar.classList.toggle('hidden');
   window.addEventListener('click', windowOnClick);
 };
+//
+
+// add link to the logo of the website to homepage
+// also set up the changing of page when a sidebar button is clicked
+const homepage = document.querySelector('main').innerHTML;
+const siteLogo = document.querySelector('header img');
+
+const main = document.querySelector('main');
+const sidebarsBtn = document.querySelectorAll('aside button');
+
+function unchild(parent) {
+  let child = parent.firstChild;
+  while (child) {
+    child.remove();
+    child = parent.firstChild;
+  }
+}
+
+siteLogo.onclick = () => {
+  unchild(main);
+  main.innerHTML = homepage;
+};
+
+sidebarsBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    unchild(main);
+  });
+});
 //
