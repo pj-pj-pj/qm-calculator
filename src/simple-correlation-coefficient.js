5810ac701017003dea3a6e3fe65b59390c3df6ed
pj-01 <96671928+pj-pj-pj@users.noreply.github.com>
2023-11-14 18:07:45 +0800

    refactor

--- src/simple-correlation-coefficient.js
+++ src/simple-correlation-coefficient.js
@@ -5,8 +5,9 @@ function calculateSimpleCorrelation(xData, yData) {
   const summationX = xData.reduce((acc, val) => acc + val, 0);
   const summationY = yData.reduce((acc, val) => acc + val, 0);
 
-  let xyData = [], xSquaredData = [], ySquaredData = [];
-  for (let i = 0; i < xData.length; i++) {
+  const xyData = []; const xSquaredData = []; const
+    ySquaredData = [];
+  for (let i = 0; i < xData.length; i += 1) {
     xyData[i] = xData[i] * yData[i];
     xSquaredData[i] = xData[i] ** 2;
     ySquaredData[i] = yData[i] ** 2;
@@ -17,28 +18,38 @@ function calculateSimpleCorrelation(xData, yData) {
   const summationYSquared = ySquaredData.reduce((acc, val) => acc + val, 0);
   const n = xData.length;
 
-  const simpleCorrelation = (summationXY - ((summationX * summationY)/n)) / (Math.sqrt((summationXSquared - ((summationX ** 2)/n))*(summationYSquared - ((summationY ** 2)/n))));
+  const xSquaredDividedByN = summationX ** 2 / n;
+  const ySquaredDividedByN = summationY ** 2 / n;
 
+  const denominator = Math.sqrt(
+    (summationXSquared - xSquaredDividedByN) * (summationYSquared - ySquaredDividedByN),
+  );
+  const numerator = summationXY - ((summationX * summationY) / n);
+  const simpleCorrelation = numerator / denominator;
   return limitDecimalPoints(simpleCorrelation, 3);
 }
 
 function getNature(value) {
   if (value < 0) return 'indirect';
   if (value > 0) return 'direct';
+  return '';
 }
 
 function getStrength(value) {
   const rawValueSC = Math.abs(value);
-  if (rawValueSC === 1) return 'perfect';
-  if (rawValueSC > 0 && rawValueSC <= 0.25) {return 'weak'}
-  else if (rawValueSC > 0.25 && rawValueSC <= 0.75) {return 'intermediate'}
-  else if (rawValueSC > 0.75 && rawValueSC < 1) {return 'strong'}
+  if (rawValueSC > 0 && rawValueSC <= 0.25) return 'weak';
+  if (rawValueSC > 0.25 && rawValueSC <= 0.75) return 'intermediate';
+  if (rawValueSC > 0.75 && rawValueSC < 1) return 'strong';
+  return '';
 }
 
 // eto yung main method para sa label niya
 function getFinalLabel(value) {
   if (value === 0) return 'no relation';
-  return `${getNature(value)} ${getStrength(value)} correlation`
+  if (value === 1 || value === -1) return 'perfect correlation';
+  return `${getNature(value)} ${getStrength(value)} correlation`;
 }
 
-module.exports = {calculateSimpleCorrelation, getNature, getStrength, getFinalLabel};
+module.exports = {
+  calculateSimpleCorrelation, getNature, getStrength, getFinalLabel,
+};
--- src/spearman-rank.js
+++ src/spearman-rank.js
@@ -3,8 +3,8 @@ const { limitDecimalPoints } = require('./mean-median-mode');
 
 function rank(data) {
   const sortedDescending = data.sort().reverse();
-  let rankData = [];
-  for (let i = 0; i < sortedDescending.length; i++) {
+  const rankData = [];
+  for (let i = 0; i < sortedDescending.length; i += 1) {
     rankData[i] = sortedDescending.indexOf(data[i]) + 1;
   }
   return rankData;
@@ -12,17 +12,18 @@ function rank(data) {
 
 function calculateSpearmanRank(xData, yData) {
   const n = xData.length;
-  let xRankData = rank(xData), yRankData = rank(yData), diData = [];
-  for (let i = 0; i < n; i++) {
+  const xRankData = rank(xData); const yRankData = rank(yData); const diData = [];
+  for (let i = 0; i < n; i += 1) {
     diData[i] = xRankData[i] - yRankData[i];
   }
-  let diSquaredData = diData.map((num) => num ** 2);
-  const diSquaredSummation = diSquaredData.reduce((acc, val) => acc + val, 0);  
+  const diSquaredData = diData.map((num) => num ** 2);
+  const diSquaredSummation = diSquaredData.reduce((acc, val) => acc + val, 0);
   const spearmanRank = 1 - ((6 * diSquaredSummation) / (n * ((n ** 2) - 1)));
 
   return limitDecimalPoints(spearmanRank, 3);
 }
 
-// pwede mo nalang din gamitin yung getNature tsaka getStrength method dun sa simple-correlation module para dito sa spearman
+// pwede mo nalang din gamitin yung getNature tsaka getStrength method
+// dun sa simple-correlation module para dito sa spearman
 
-module.exports = {rank, calculateSpearmanRank};
+module.exports = { rank, calculateSpearmanRank };
